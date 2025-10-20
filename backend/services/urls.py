b27ab7aecb5e24f6d3331b4db9ff968905ce3b8d
mclem6 <mclem6@uic.edu>
2025-10-19 23:49:02 -0500

    comment edit

--- backend/services/urls.py
+++ backend/services/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 from .views import CustomerProfileListView, ServiceProviderProfileListView
 
 urlpatterns = [
+    #List all customers or create a new one (GET, POST)
     path('customers/', CustomerProfileListView.as_view(), name='customer-list'),
+    #List all providers or create a new one (GET, POST)
     path('providers/', ServiceProviderProfileListView.as_view(), name='provider-list')
 ]
