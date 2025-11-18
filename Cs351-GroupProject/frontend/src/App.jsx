6c430cf253a7c1b769c0eaa92851d94f1fa55ef0
Felix Dominguez <Fd20586@gmail.com>
2025-11-17 19:52:56 -0600

    Update frontend UI with latest changes

--- Cs351-GroupProject/frontend/src/App.jsx
+++ Cs351-GroupProject/frontend/src/App.jsx
@@ -1,18 +1,12 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar/Navbar.jsx";
-import { Hero } from "./Components/Hero/Hero.jsx";
-import Categories from "./Components/Categories/Categories.jsx";
-import Programs from "./Components/Programs/Programs.jsx";
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Navbar from './Components/Navbar/Navbar.jsx'
+import { Hero } from './Components/Hero/Hero.jsx'
+import Categories from './Components/Categories/Categories.jsx'
+import Programs from './Components/Programs/Programs.jsx'
 import Verify from "./Pages/Verify.jsx";
 import Provider from "./Components/Provider/Provider.jsx";
-import ProviderProfile from "./Components/Provider/ProviderProfile.jsx";
-import AboutUIC from "./Pages/Profile/AboutUIC.jsx";
-import Profile from "./Pages/Profile/Profile.jsx";
-import Feedback from "./Pages/Feedback.jsx";
-import AccountDetails from "./Pages/Profile/AccountDetails.jsx";
-import Settings from "./Pages/Profile/Settings.jsx";
-import Reviews from "./Pages/Profile/Reviews.jsx";
+import ProviderProfile from "./Components/Provider/ProviderProfile.jsx"; // Add this
 
 function Home() {
   return (
@@ -22,7 +16,7 @@ function Home() {
       <Categories />
       <Programs />
     </div>
-  );
+  )
 }
 
 export default function App() {
@@ -32,14 +26,8 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/verify" element={<Verify />} />
         <Route path="/provider/:id" element={<Provider />} />
-        <Route path="/become-provider" element={<ProviderProfile />} />
-        <Route path="/about-uic" element={<AboutUIC />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/feedback" element={<Feedback />} />
-        <Route path="/account-details" element={<AccountDetails />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/become-provider" element={<ProviderProfile />} /> {/* Add this route */}
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
